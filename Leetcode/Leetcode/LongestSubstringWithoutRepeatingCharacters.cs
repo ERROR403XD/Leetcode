30036b71af9d55d25b569e86fd465eb55a743daa
ERROR403XD <wangchu11235@gmail.com>
2017-12-06 17:35:00 +0800

    try another ，still not  good

--- Leetcode/Leetcode/LongestSubstringWithoutRepeatingCharacters.cs
+++ Leetcode/Leetcode/LongestSubstringWithoutRepeatingCharacters.cs
@@ -11,42 +11,19 @@ namespace Leetcode
         public int LengthOfLongestSubstring(string s)
         {
             if (s.Length <= 1) return s.Length;
-            int areaMax_start = 0;
-            int areaMax_end = 0;
-            int endMax_start = 0;
-            int flag;
             int endFlag = s.Length - 1;
-            for(flag = 1;flag<=endFlag;flag++)
+            int areaMax = 0;
+            int endMaxStart = 0;
+            for(int flag = 1;flag<=endFlag;flag++)
             {
-                if(areaMax_end == flag-1)
+                int tempStart = FindNewEndMaxStart(s, flag);
+                if (flag - tempStart >= areaMax)
                 {
-                    if(JudgeExist(s,s[flag],endMax_start,flag-1)==-1)
-                    {
-                        areaMax_end++;
-                    }
-                    else
-                    {
-                        endMax_start = FindNewEndMaxStart(s, flag);
-                    }
-                }
-                else
-                {
-                    if(JudgeExist(s,s[flag],endMax_start,flag-1)==-1)
-                    {
-                        if((flag - endMax_start)>=(areaMax_end-areaMax_start))
-                        {
-                            areaMax_start = endMax_start;
-                            areaMax_end = flag;   
-                        }
-                    }
-                    else
-                    {
-                        endMax_start = FindNewEndMaxStart(s, flag);
-                    }
-
-                }
-            }        
-            return areaMax_end - areaMax_start + 1;
+                    areaMax = flag - tempStart + 1;
+                    endMaxStart = tempStart;
+                }      
+            }
+            return areaMax;  
         }
         public int FindNewEndMaxStart(string s,int flag)
         {
@@ -74,7 +51,7 @@ namespace Leetcode
         static void Main(string[] args)
         {
             LongestSubstringWithoutRepeatingCharacters test = new LongestSubstringWithoutRepeatingCharacters();
-            string testString = "aaaaaaaaa";
+            string testString = "asdcghtrgfasmmhjfdf";
             Console.WriteLine(test.LengthOfLongestSubstring(testString).ToString());
 
             Console.ReadKey();
