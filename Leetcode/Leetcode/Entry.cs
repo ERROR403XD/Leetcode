0966e35a4cc0e5839f3a4165c787d5f57cb31606
ERROR403XD <wangchu11235@gmail.com>
2017-12-11 14:57:28 +0800

    写完了最长回文子串

--- Leetcode/Leetcode/Entry.cs
+++ Leetcode/Leetcode/Entry.cs
@@ -10,22 +10,19 @@ namespace Leetcode
     {
         static void Main(string[] args)
         {
-            MedianofTwoSortedArrays test = new MedianofTwoSortedArrays();
-            string testString = "abba";
+            LongestPalindromicSubstring test = new LongestPalindromicSubstring();
+            string testString = "babad";
             int[] testArray = new int[5] { 2, 6, 7, 12, 35 };
             int[] testArrayM = new int[] {3};
             int[] testArrayN = new int[] {1,2};
             double pi = 3.1415926;
-
-            double length = 2.7 * 16;
-            double area = 0.002 * 0.072;
-            double rate = 2.82 * Math.Pow(10, -8);
-            Console.WriteLine((rate*length / area).ToString());
+            char[] c = new char[2];
+            c[1] = 'a';
+            test.LongestPalindrome(testString);
             //Console.WriteLine(5550000000 * ((2 * pi * pi * 0.1 * 0.01 * 0.01) - (2 * pi * pi * 0.1 * 0.007 * 0.007)));
             //Console.WriteLine(3110000000 * (pi*0.076*0.076-pi*0.074*0.074)*0.15);
             //Console.WriteLine(test.FindKth(testArray,1).ToString());          
-            //Console.WriteLine(test.FindEqual(testArrayM,testArrayN,0,testArrayM.Length-1,testArrayM.Length,testArrayN.Length).ToString());
-            Console.WriteLine(test.FindMedianSortedArrays(testArrayM, testArrayN, true));
+            //Console.WriteLine(test.FindEqual(testArrayM,testArrayN,0,testArrayM.Length-1,testArrayM.Length,testArrayN.Length).ToString()); 
             Console.ReadKey();
         }
     }
--- Leetcode/Leetcode/LongestPalindromicSubstring.cs
+++ Leetcode/Leetcode/LongestPalindromicSubstring.cs
@@ -10,7 +10,97 @@ namespace Leetcode
     {
         public string LongestPalindrome(string s)
         {
-            return "";
+            if (s == null) return "";
+            if (s.Length <= 1) return s;
+            char flag = default(char);
+            string source = flag.ToString();
+            for(int i = 0;i<s.Length;i++)
+            {
+                source += s[i]+ flag.ToString();
+            }
+            int[] digs = new int[source.Length];
+
+            int currentMaxPos = 0;
+            int currentMaxBoundary = 0;
+            int currentMaxBoundary_ = 0;
+
+            int maxPos = 0;
+            int maxVal = 0;
+            for(int i = 0;i<source.Length;i++)
+            {
+                int currentBoundary = 0;
+                
+                if(i<=currentMaxBoundary)
+                {
+                    int opposite = currentMaxPos - (i - currentMaxPos);
+                    digs[i] = digs[opposite];
+                    if (opposite - digs[opposite]> currentMaxBoundary_)
+                    {
+                        continue;
+                    }
+                }  
+
+                while(true)
+                {
+                    if(i-currentBoundary-1<0||i+currentBoundary+1>=digs.Length)
+                    {
+                        break;
+                    }
+                    if(source[i+currentBoundary+1]==source[i-currentBoundary-1])
+                    {
+                        currentBoundary++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    digs[i] = currentBoundary;
+
+                    if(currentBoundary>maxVal)
+                    {
+                        maxPos = i;
+                        maxVal = currentBoundary;
+                    }
+
+                    if(maxPos+maxVal>=source.Length)
+                    {
+                        break;
+                    }
+                    if(i+currentBoundary>currentMaxBoundary)
+                    {
+                        currentMaxPos = i;
+                        currentMaxBoundary = i + currentBoundary;
+                        currentMaxBoundary_ = i - currentBoundary;
+                    }
+                }
+            }
+
+            string res = s.Substring(maxPos/2-maxVal/2,maxVal);
+            /*
+            if(maxPos%2==0)
+            {
+                res = "";
+            }
+            else
+            {
+                res = source[maxPos].ToString();
+            }
+            for(int i =maxPos+1;i<=maxPos+maxVal;i++)
+            {
+                if(source[i] != default(char))
+                {
+                    res = source[i].ToString() + res + source[i].ToString();
+                }
+            }
+             */
+            for (int i = 0;i<digs.Length;i++)
+            {
+                Console.Write(digs[i].ToString() + " ");
+            }   
+            Console.WriteLine();
+            Console.WriteLine(source);
+            Console.WriteLine(res);
+            return res;
         }
     }
 }
