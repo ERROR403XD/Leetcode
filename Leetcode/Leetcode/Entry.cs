b17f08a54a7a4e6e1501e7e5467b4d8e185372de
ERROR403XD <wangchu11235@gmail.com>
2017-12-11 15:24:35 +0800

    反转整数

--- Leetcode/Leetcode/Entry.cs
+++ Leetcode/Leetcode/Entry.cs
@@ -10,19 +10,17 @@ namespace Leetcode
     {
         static void Main(string[] args)
         {
-            LongestPalindromicSubstring test = new LongestPalindromicSubstring();
+            ReverseInteger test = new ReverseInteger();
             string testString = "babad";
             int[] testArray = new int[5] { 2, 6, 7, 12, 35 };
             int[] testArrayM = new int[] {3};
             int[] testArrayN = new int[] {1,2};
+
+            int testInt = 123;
             double pi = 3.1415926;
-            char[] c = new char[2];
-            c[1] = 'a';
-            test.LongestPalindrome(testString);
-            //Console.WriteLine(5550000000 * ((2 * pi * pi * 0.1 * 0.01 * 0.01) - (2 * pi * pi * 0.1 * 0.007 * 0.007)));
-            //Console.WriteLine(3110000000 * (pi*0.076*0.076-pi*0.074*0.074)*0.15);
-            //Console.WriteLine(test.FindKth(testArray,1).ToString());          
-            //Console.WriteLine(test.FindEqual(testArrayM,testArrayN,0,testArrayM.Length-1,testArrayM.Length,testArrayN.Length).ToString()); 
+
+            test.Reverse(testInt);
+
             Console.ReadKey();
         }
     }
---
+++ Leetcode/Leetcode/ReverseInteger.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    class ReverseInteger
+    {
+        public int Reverse(int x)
+        {
+            if (x > -10 && x < 10) return x;
+            if (x == -2147483648) return 0;
+            bool isNeg = (x < 0) ? true : false;
+            if (isNeg) x = -x;
+
+            int[] compare = new int[] { 2, 1, 4, 7, 4, 8, 3, 6, 4, 7 };
+            int[] source = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+            int length = 0;
+
+            while (true)
+            {
+                source[length] = x % 10;
+                length++;
+                if (x < 10) break;
+                x /= 10;
+            }
+            int res = 0;
+            bool danger = (length == 10) ? true : false;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (danger)
+                {
+                    if (source[i] > compare[i])
+                    {
+                        return 0;
+                    }
+                    if (source[i] < compare[i])
+                    {
+                        danger = false;
+                    }
+                }
+                res += source[i] * Pow(10, length - i - 1);
+
+            }
+            return (isNeg) ? -res : res;
+
+        }
+
+        public int Pow(int basement, int top)
+        {
+            if (top == 0)
+            {
+                return 1;
+            }
+            if (top == 1)
+            {
+                return basement;
+            }
+            return Pow(basement, top / 2) * Pow(basement, top - top / 2);
+        }
+    }
+}
