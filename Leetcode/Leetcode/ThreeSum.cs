fd8bdfeeaec0238e157225270a4910b8909162f5
403-office <wangchu11235@foxmail.com>
2017-12-20 18:01:00 +0800

    写完了3sum，双指针法

--- Leetcode/Leetcode/ThreeSum.cs
+++ Leetcode/Leetcode/ThreeSum.cs
@@ -10,29 +10,143 @@ namespace Leetcode
     {
         public IList<IList<int>> ThreeSum(int[] nums)
         {
-            List<IList<int>> resList = new List<IList<int>>();
+            List<IList<int>> res = new List<IList<int>>();
+            Sort(nums);
 
-            for(int i = 0; i < nums.Length-2; i++)
+            for (int i = 0; i < nums.Length - 2; i++)
             {
-                int res = nums[i];
-                for(int j = i+1;j<nums.Length-1;j++)
+                if (i == 0 || nums[i] != nums[i - 1])
                 {
-                    int res2 = res + nums[j];
-                    for(int k = j+1;k<nums.Length;k++)
-                    {
-                        if(nums[k]==-res2)
-                        {
-                            List<int> tempres = new List<int>();
-                            tempres.Add(nums[i]);
-                            tempres.Add(nums[j]);
-                            tempres.Add(nums[k]);
+                    int left = i + 1;
+                    int right = nums.Length - 1;
 
-                            resList.Add(tempres as IList<int>);
+                    while (left < right)
+                    {
+                        int temp = nums[i] + nums[left] + nums[right];
+                        if (temp == 0)
+                        {
+                            List<int> tempList = new List<int>();
+                            tempList.Add(nums[i]);
+                            tempList.Add(nums[left]);
+                            tempList.Add(nums[right]);
+                            res.Add(tempList as IList<int>);
+                            while (left < right)
+                            {
+                                left++;
+                                if (nums[left] != nums[left - 1])
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                        else if (temp < 0)
+                        {
+                            while (left < right)
+                            {
+                                left++;
+                                if (nums[left] != nums[left - 1])
+                                {
+                                    break;
+                                }   
+                            }
+
+                        }
+                        else if (temp > 0)
+                        {
+                            while (left < right)
+                            {
+                                right--;
+                                if (nums[right] != nums[right + 1])
+                                {
+                                    break;
+                                }  
+                            }
                         }
                     }
                 }
             }
-            return resList as IList<IList<int>>;
-        }                                                     
+            return res as IList<IList<int>>;
+        }
+        /*
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            List<IList<int>> res = new List<IList<int>>();
+            Sort(nums);
+
+            for(int i = 0;i<nums.Length-2;i++)
+            {
+                if(i==0 || nums[i]!=nums[i-1])
+                {
+                    List<int[]> temp = Sum2(nums, i + 1, -nums[i]);
+                    foreach (int[] tempPair in temp)
+                    {
+                        List<int> resTemp = new List<int>();
+                        resTemp.Add(nums[i]);
+                        resTemp.Add(tempPair[0]);
+                        resTemp.Add(tempPair[1]);
+                        res.Add(resTemp as IList<int>);
+                    }
+                }
+
+            }     
+
+            return res as IList<IList<int>>;  
+        }   */
+        private List<int[]> Sum2(int[]nums,int start,int target)
+        {
+            List<int> restore = new List<int>();
+            List<int> already = new List<int>();
+            List<int[]> res = new List<int[]>();
+
+            for(int i = start;i<nums.Length;i++)
+            {
+                if(restore.Contains(nums[i]))
+                {   
+                    if(!already.Contains(nums[i]))
+                    {
+                        res.Add(new int[2] { target - nums[i], nums[i] });
+                        already.Add(nums[i]);
+                    }   
+                }
+                else
+                { 
+                    if(!restore.Contains(target-nums[i]))
+                    {
+                        restore.Add(target - nums[i]);
+                    }    
+                }
+            }
+
+            return res;
+        }
+        private void Sort(int[] nums)
+        {
+            Sort(nums, 0, nums.Length - 1);
+        }
+        private void Sort(int[] nums,int start,int end)
+        {
+            if (end - start < 1) return;
+
+            int compare = nums[start];
+            int comparePos = start;
+                                 
+            for(int flag = start+1;flag<=end;flag++)
+            {
+                if(nums[flag]<=compare)
+                {
+                    Turn(ref nums[comparePos + 1], ref nums[flag]);
+                    comparePos++;  
+                }    
+            }
+            Turn(ref nums[start], ref nums[comparePos]);
+            Sort(nums, start, comparePos - 1);
+            Sort(nums, comparePos + 1, end);
+        }
+        private void Turn(ref int a,ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
     }
 }
