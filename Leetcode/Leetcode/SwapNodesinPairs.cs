b88bf7874a5d51bd626de0c44d88ed45667559b5
ERROR403XD <30397081+ERROR403XD@users.noreply.github.com>
2017-12-21 22:48:45 +0800

    swap listnode in pairs

---
+++ Leetcode/Leetcode/SwapNodesinPairs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    class SwapNodesinPairs
+    {
+        public ListNode SwapPairs(ListNode head)
+        {
+            if (head == null) return null;
+            ListNode first = head;
+            ListNode second = head.next;
+            if (second == null) return head;
+            ListNode front = second.next;
+            second.next = first;
+            first.next = SwapPairs(front);
+            return second;
+        }
+    }
+}
