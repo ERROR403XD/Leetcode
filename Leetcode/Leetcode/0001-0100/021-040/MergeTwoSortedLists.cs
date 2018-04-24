883ab5494920a1ded1ae28820d42902bad98eae2
ERROR403XD <30397081+ERROR403XD@users.noreply.github.com>
2018-04-25 00:40:59 +0800

    记得重新写一个KMP

--- Leetcode/Leetcode/0001-0100/021-040/MergeTwoSortedLists.cs
+++ Leetcode/Leetcode/0001-0100/021-040/MergeTwoSortedLists.cs
@@ -13,38 +13,38 @@ namespace Leetcode
             if (l1 == null) return l2;
             if (l2 == null) return l1;
 
-            ListNode head = new ListNode(0);
-            ListNode current = head;
-
-            while(true)
+            ListNode head = null;
+            ListNode current = null;
+            if(l1.val<=l2.val)
             {
-                if(l1.val<l2.val)
+                head = l1;
+                l1 = l1.next;
+            }
+            else
+            {
+                head = l2;
+                l2 = l2.next;
+            }
+            current = head;
+            while(l1!=null&&l2!=null)
+            {
+                if (l1.val < l2.val)
                 {
                     current.next = l1;
                     l1 = l1.next;
-                    current = current.next;
-
-                    if(l1==null)
-                    {
-                        current.next = l2;
-                        break;
-                    }     
                 }
                 else
                 {
                     current.next = l2;
                     l2 = l2.next;
-                    current = current.next;
-
-                    if (l2 == null)
-                    {
-                        current.next = l1;
-                        break;
-                    }  
-                }  
+                }
+                current = current.next;
             }
+            if (l1 == null) current.next = l2;
+            if (l2 == null) current.next = l1;
 
-            return head.next;
+
+            return head;
         }
         public ListNode MergeKLists(ListNode[] lists)
         {
---
+++ Leetcode/Leetcode/Linked List Cycle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    class Linked_List_Cycle
+    {
+        public bool HasCycle(ListNode head)
+        {
+            if (head == null) return false;
+            HashSet<ListNode> set = new HashSet<ListNode>();
+            while (head != null)
+            {
+                if (!set.Contains(head))
+                {
+                    set.Add(head);
+                    head = head.next;
+                }
+                else return true;
+            }
+            return false;
+
+        }
+        public bool HasCycle_(ListNode head)
+        {
+            if (head == null) return false;
+            ListNode slow = head, fast = head;
+            while(fast!=null&&fast.next!=null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+                if (slow == fast) return true;
+            }
+            return false;
+        }
+        public ListNode DetectCycle(ListNode head)
+        {
+            if (head == null) return null;
+            HashSet<ListNode> set = new HashSet<ListNode>();
+            while(head!=null)
+            {
+                if (set.Contains(head)) return head;
+                set.Add(head);
+                head = head.next;
+            }
+            return null;
+        }
+    }
+}
